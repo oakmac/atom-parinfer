1f86a112df8ee98ff23f5af76701e51c53cf9787
Chris Oakman <chris@oakmac.com>
2015-11-07 02:56:23 -0600

    move parinfer library modification to this repo; cleanup in atom-parinfer.js

---
+++ parinfer-cljs/README.md
@@ -0,0 +1,8 @@
+# NOTE: this is NOT the full version of Parinfer
+
+This is a chopped up version used for the atom extension. To see the real deal
+head on over to Shaun LeBron's [Parinfer] repo.
+
+Run `lein cljsbuild once` from this folder to build `./lib/parinfer.js`.
+
+[Parinfer]:https://github.com/shaunlebron/parinfer
---
+++ parinfer-cljs/project.clj
@@ -0,0 +1,23 @@
+(defproject parinfer-lib "0.1.0-SNAPSHOT"
+  :description "chopped up version of parinfer for the atom extension"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild
+    {:builds
+      [{:id "lib"
+        :source-paths ["src"]
+        :compiler {:output-to "../lib/parinfer.js"
+                   :optimizations :simple
+                   :language-in :ecmascript5
+                   :language-out :ecmascript5
+                   :target :nodejs
+                   :hashbang false
+                   :pretty-print true}}]})
